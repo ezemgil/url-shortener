444af4bcf27bab1d7615306451b7d6319c7b6d07
ezemg <ezemgil@gmail.com>
2025-01-30 13:02:15 -0300

    Added size constraint on UserDTO.name, UrlDTO.name and UrlDTO.originalUrl

--- src/main/java/ezemgil/urlShortener/dto/UrlDTO.java
+++ src/main/java/ezemgil/urlShortener/dto/UrlDTO.java
@@ -3,6 +3,7 @@ package ezemgil.urlShortener.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
@@ -16,12 +17,14 @@ import static lombok.AccessLevel.PRIVATE;
 @FieldDefaults(level = PRIVATE)
 public class UrlDTO {
     @NotBlank(message = "Name is mandatory")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     String name;
 
     String shortKey;
 
     @NotBlank(message = "Original URL is mandatory")
     @Pattern(regexp = "^(http|https)://.*$", message = "Invalid URL")
+    @Size(min = 3, max = 500, message = "URL must be between 3 and 500 characters")
     String originalUrl;
 
     LocalDateTime createdAt;
--- src/main/java/ezemgil/urlShortener/dto/UserDTO.java
+++ src/main/java/ezemgil/urlShortener/dto/UserDTO.java
@@ -2,12 +2,12 @@ package ezemgil.urlShortener.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import static lombok.AccessLevel.PRIVATE;
 
@@ -18,6 +18,7 @@ public class UserDTO {
     Long id;
 
     @NotBlank(message = "Name is mandatory")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     String name;
 
     @NotBlank(message = "Email is mandatory")
